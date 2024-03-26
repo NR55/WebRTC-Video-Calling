6cd0f8a3bdd9fc31e70c53436e50bcbc48bbd3db
sanjeevholla26 <sanjeevholla26@gmail.com>
2024-03-26 14:54:58 +0530

    [UI]: Add navbar

---
+++ public/navbar.js
@@ -0,0 +1,4 @@
+const nav=document.getElementById("navbar");
+nav.innerHTML += "<a href=\" / \" id=\"home\" title=\"Home\"><img src=\" /home.png\" alt=\"Home\"></a>"
+nav.innerHTML += "<a href=\"/profile\" id=\"profile\" title=\"Profile\">Profile</a>"
+nav.innerHTML += "<a href=\"/logout\" id=\"logout\" title=\"Logout\"><img src=\" /logout.png\" alt=\"Logout\"></a>"
